d39fafea65e3c1ae1a676e56a2c618507facd0eb
Your Name <you@example.com>
2019-12-07 22:11:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # geth_peers_geo2influx
 A python script to list ethereum geth peers IPs, get their geolocation and send it to InfluxDB
+
+## dependencies
+- **geoip2** python library  
+  *apt-get install python-geoip2* or  
+  *pip install geoip2*  
+- **influxdb** python library  
+  *apt-get install python-influxdb* or  
+  *pip install influxdb*  
+- **GeoLite2 City** free database  
+  [https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz](https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz)
+
+
+## quick start
+- edit the file and make sure paths to files are correct
+- the script requires geth server to be running
+- geolocation information is sent to Influx database
+- the data can be used by eg. Grafana Worldmap plugin
+- the script should be executed periodically as a crob job
+
+
+## data format
+
+    > select * from geth_peers_geo limit 5;
+    name: geth_peers_geo
+    time                city                               lat     lon
+    ----                ----                               ---     ---
+    1575737543016009984 5.187.1.xyz (Frankfurt am Main DE) 50.1188 8.6843
+    1575737543016866048 13.229.153.xyz (Singapore SG)      1.2929  103.8547
+    1575737543017404928 93.115.24.xyz (None LT)             56      24
+    1575737543027055104 47.52.35.xyz (None CN)             22.25   114.1667
+    1575737543027645952 95.216.246.xyz (None FI)           60.1717 24.9349
+
+## use case
+Here's a [screenshot](https://ethbian.org/images/grafana_worldmap_plugin.png) of Grafana Worldmap plugin using provided data.
+
+Pull requests are more than welcome if you're fixing something
---
+++ geth_peers_geo2influx.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+"""
+Use the script to:
+- get IPs of peers connected to Ethereum geth daemon
+- get geolocation information of these addresses
+- send it to InfluxDB database
+...so that it could be used with eg. Grafana Worldmap plugin
+https://ethbian.org | https://github.com/ethbian/ethbian/
+"""
+
+import os
+import datetime
+import subprocess
+
+try:
+    import geoip2.database
+except ImportError:
+    raise ImportError('\n\n cannot find Python library: geoip2\n' +
+                      ' try executing: pip install geoip2 or apt-get install python-geoip2')
+try:
+    import influxdb
+except ImportError:
+    raise ImportError('\n\n cannot find Python library: influxdb\n' +
+                      ' try executing: pip install influxdb or apt-get install python-influxdb')
+
+SERVICE = 'geth'
+GETH_PATH = '/usr/local/bin/geth/geth'
+IPC_PATH = '/mnt/ssd/datadir/geth.ipc'
+GEODB_PATH = '/usr/local/lib/collectd/geolite_city.mmdb'
+
+DB_HOST = 'localhost'
+DB_NAME = 'collectd'
+DB_PORT = 8086
+DB_TABLE = 'geth_peers_geo'
+
+for f in [GETH_PATH, IPC_PATH, GEODB_PATH]:
+    if not os.path.exists(f):
+        raise SystemExit('File {} does not exist.'.format(f))
+
+geth_running = subprocess.call(
+    'systemctl is-active --quiet {}'.format(SERVICE), shell=True)
+if geth_running != 0:
+    raise SystemExit('Service {} is not running.'.format(SERVICE))
+
+try:
+    db_client = influxdb.InfluxDBClient(host=DB_HOST, port=DB_PORT, database=DB_NAME)
+except Exception as e:
+    raise SystemExit('Error getting peer list: {}'.format(e))
+
+try:
+    admin_peers = subprocess.check_output('{} {}{} {}'.\
+                          format(GETH_PATH, 'attach ipc:', IPC_PATH, '--exec admin.peers'),\
+                          shell=True)
+except Exception as e:
+    raise SystemExit('Error connecting to database: {}'.format(e))
+
+try:
+    geodb = geoip2.database.Reader(GEODB_PATH)
+except Exception as e:
+    raise SystemExit('Error opening geodb file: {}'.format(e))
+peers = []
+
+for line in admin_peers.splitlines():
+    peer = {}
+    if 'remoteAddress' in line:
+        ip = line.split('"')[1].split(':')[0]
+        if ip:
+            geo = geodb.city(ip)
+            peer['measurement'] = DB_TABLE
+            peer['time'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            peer['fields'] = {
+                'lat': geo.location.latitude,
+                'lon': geo.location.longitude,
+                'city': '{} ({} {})'.format(ip, geo.city.name, geo.registered_country.iso_code)
+            }
+        peers.append(peer)
+
+try:
+    db_client.write_points(peers)
+except Exception as e:
+    print 'Error writing to database: {}'.format(e)
+else:
+    print 'Sent {} geth peers to database.'.format(len(peers))
+
+db_client.close()
